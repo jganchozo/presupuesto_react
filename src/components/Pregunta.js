5cadb9e1be3624b5ea671a072fbdb89a7263613e
jganchozo <jganchozo@outlook.com>
2021-03-01 18:18:13 -0500

    Error Component

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
 
 const Pregunta = () => {
+
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    const definirPresupuesto = (e) => {
+        guardarCantidad(parseInt(e.target.value, 10));
+    }
+
+    const agregarPresupuesto = (e) => {
+        e.preventDefault();
+
+        //validar
+        if (cantidad < 1 || isNaN(cantidad)) {
+            guardarError(true);
+            return;
+        }
+
+        //si se pasa la validacion
+        guardarError(false);
+    }
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            { error ? <Error mensaje="El Presupuesto es Incorrecto"/> : null }
+
+            <form onSubmit={agregarPresupuesto}>
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input 
