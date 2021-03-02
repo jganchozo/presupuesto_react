f63a559f6221667e694fe5e0cf70a9c72019559e
jganchozo <jganchozo@outlook.com>
2021-03-02 15:40:37 -0500

    PropTypes

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Gasto = ({gasto}) => {
 
@@ -11,6 +12,10 @@ const Gasto = ({gasto}) => {
             </p>
         </li>
      );
+}
+
+Gasto.propTypes = {
+    gasto: PropTypes.object.isRequired
 }
  
 export default Gasto;
