f73ce55a879130534e5eef4d0db97126ed071f21
jganchozo <jganchozo@outlook.com>
2021-03-02 08:09:52 -0500

    Formulario Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import Pregunta from './components/Pregunta';
+import React, { useState } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  //definir state 
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+  const [pregunta, actualizarPregunta] = useState(true);
+
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
       </header>
       <div className="contenido-principal contenido">
-        <Pregunta />
+
+      {pregunta 
+      ?
+      (
+        <Pregunta 
+          guardarPresupuesto={guardarPresupuesto}
+          guardarRestante={guardarRestante}
+          actualizarPregunta={actualizarPregunta}
+        />
+      )
+      :
+      (
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      )}        
+
       </div>
     </div>
   );
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error';
 
-const Pregunta = () => {
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
 
     const [cantidad, guardarCantidad] = useState(0);
     const [error, guardarError] = useState(false);
@@ -21,6 +21,9 @@ const Pregunta = () => {
 
         //si se pasa la validacion
         guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
     }
 
     return ( 
